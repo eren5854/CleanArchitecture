e7fbcce3d67f61292047dc971293a03fe1d4ef85
eren5854 <ihsanerendelibas@hotmail.com>
2025-05-11 00:01:40 +0300

    .

---
+++ CleanArchitecture.Application/Employees/EmployeeGetAllQuery.cs
@@ -0,0 +1,41 @@
+﻿using CleanArchitecture.Domain.Abstractions;
+using CleanArchitecture.Domain.Employees;
+using MediatR;
+
+namespace CleanArchitecture.Application.Employees;
+public sealed record EmployeeGetAllQuery() : IRequest<IQueryable<EmployeeGetAllQueryResponse>>;
+
+public sealed class EmployeeGetAllQueryResponse : EntityDto
+{
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public DateOnly DateOfBirth { get; set; }
+    public decimal Salary { get; set; }
+    public string TCNo { get; set; } = default!;
+}
+
+internal sealed class EmployeeGetAllQueryHandler(
+    IEmployeeRepository employeeRepository) : IRequestHandler<EmployeeGetAllQuery, IQueryable<EmployeeGetAllQueryResponse>>
+{
+    public async Task<IQueryable<EmployeeGetAllQueryResponse>> Handle(EmployeeGetAllQuery request, CancellationToken cancellationToken)
+    {
+        var employees = await employeeRepository.GetAll().Select(s => new EmployeeGetAllQueryResponse
+        {
+            FirstName = s.FirstName,
+            LastName = s.LastName,
+            DateOfBirth = s.DateOfBirth,
+            Salary = s.Salary,
+            TCNo = s.PersonalInformation.TCNo
+
+        })
+        return employees.Select(e => new EmployeeGetAllQueryResponse
+        {
+            Id = e.Id,
+            FirstName = e.FirstName,
+            LastName = e.LastName,
+            DateOfBirth = e.DateOfBirth,
+            Salary = e.Salary,
+            TCNo = e.PersonalInformation.TCNo
+        });
+    }
+}
---
+++ CleanArchitecture.Domain/Abstractions/EntityDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArchitecture.Domain.Abstractions;
+public abstract class EntityDto
+{
+    public Guid Id { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime CreateAt { get; set; }
+    public DateTime? UpdateAt { get; set; }
+    public DateTime? DeleteAt { get; set; }
+}
